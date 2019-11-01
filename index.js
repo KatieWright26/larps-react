fb59db6b99d9ec7a8f06d4aba636864e7591c0f6
Katie Wright <katie.wrght@gmail.com>
2019-11-01 15:49:15 +0100

    install immer, redux and react-redux and start to build out hard coded fucntionality for delete

--- index.js
+++ index.js
@@ -1,20 +1,48 @@
-import React from 'react'
-import { render } from 'react-dom'
-import App from './components/app'
+import { Provider } from 'react-redux';
+import React from 'react';
+import { render } from 'react-dom';
+import { createStore } from 'redux';
+import produce from 'immer';
+import App from './components/app';
 
-const state = {
+const initialState = {
   user: { id: 1, name: 'Katie', larpIds: [1] },
   larps: [
-    { id: 1, name: 'star trek in the park', ownerId: 1, characterIds: [1, 2, 3] },
-    { id: 3, name: 'spongebob', ownerId: 2, characterIds: [5, 7, 9] }
+    {
+      id: 1,
+      name: 'star trek in the park',
+      ownerId: 1,
+      characterIds: [1, 2, 3],
+    },
+    { id: 3, name: 'spongebob', ownerId: 2, characterIds: [5, 7, 9] },
   ],
   characters: [
     { id: 1, name: 'Scotty' },
-    { id: 1, name: 'Spok' },
-    { id: 1, name: 'Captain Sleaze' }
-  ]
+    { id: 2, name: 'Spok' },
+    { id: 3, name: 'Captain Sleaze' },
+  ],
+};
 
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case 'DELETE_LARP':
+      return produce(state, draftState => {
+        draftState.larps = draftState.larps.filter(
+          l => l.id !== action.payload
+        );
+      });
+    default:
+      return state;
+  }
 }
 
-render(<App name='larps' state={state} />, document.querySelector('main'))
-console.log('welcome to larps')
+const store = createStore(reducer);
+
+render(
+  <Provider store={store}>
+    <App name="larps" />,
+  </Provider>,
+  document.querySelector('main')
+);
+
+console.log('welcome to larps');
