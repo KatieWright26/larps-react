12d4034a4e88859a7da835d3fc0ff05e0a7bdbae
Katie Wright <katie.wrght@gmail.com>
2019-11-04 16:47:21 +0100

    install tape and write tests for happy paths of current reducer functions

--- test/index.js
+++ test/index.js
@@ -1,6 +1,135 @@
-var test = require('tape')
+const tape = require('tape');
+const { createStore } = require('redux');
+const reducer = require('../reducer');
 
-test('testing larps', function (t) {
-  t.ok(true)
-  t.end()
-})
+const initialState = {
+  larps: [
+    {
+      id: 1,
+      name: 'MiddleEarthe',
+      ownerId: 1,
+      characters: [
+        { id: 1, name: 'Samwise Gamgree' },
+        { id: 2, name: 'Peregrin Took' },
+        { id: 3, name: 'Meriadoc Brandybuck' },
+      ],
+    },
+    {
+      id: 3,
+      name: "Elves 'n' Things",
+      ownerId: 1,
+      characters: [
+        { id: 4, name: 'Galadriel' },
+        { id: 5, name: 'Elrond' },
+        { id: 6, name: 'Éowyn' },
+        { id: 5, name: 'Arwen' },
+      ],
+    },
+  ],
+};
+
+tape('Delete a LARP', function(test) {
+  const store = createStore(reducer, initialState);
+
+  store.dispatch({ type: 'DELETE_LARP', payload: 1 });
+
+  const actual = store.getState().larps;
+
+  const expected = [
+    {
+      id: 3,
+      name: "Elves 'n' Things",
+      ownerId: 1,
+      characters: [
+        { id: 4, name: 'Galadriel' },
+        { id: 5, name: 'Elrond' },
+        { id: 6, name: 'Éowyn' },
+        { id: 5, name: 'Arwen' },
+      ],
+    },
+  ];
+
+  test.deepEqual(actual, expected, 'LARP successfully deleted');
+  test.end();
+});
+
+tape('Delete a Character', function(test) {
+  const store = createStore(reducer, initialState);
+
+  store.dispatch({ type: 'DELETE_CHARACTER', payload: 4 });
+
+  const actual = store.getState().larps[1].characters;
+
+  const expected = [
+    { id: 5, name: 'Elrond' },
+    { id: 6, name: 'Éowyn' },
+    { id: 5, name: 'Arwen' },
+  ];
+
+  test.deepEqual(actual, expected, 'Character successfully deleted');
+  test.end();
+});
+
+tape('Create a LARP', function(test) {
+  const store = createStore(reducer, initialState);
+
+  store.dispatch({ type: 'CREATE_LARP', payload: { name: 'Katie' } });
+
+  const actual = store.getState().larps;
+
+  const expected = [
+    {
+      id: 1,
+      name: 'MiddleEarthe',
+      ownerId: 1,
+      characters: [
+        { id: 1, name: 'Samwise Gamgree' },
+        { id: 2, name: 'Peregrin Took' },
+        { id: 3, name: 'Meriadoc Brandybuck' },
+      ],
+    },
+    {
+      id: 3,
+      name: "Elves 'n' Things",
+      ownerId: 1,
+      characters: [
+        { id: 4, name: 'Galadriel' },
+        { id: 5, name: 'Elrond' },
+        { id: 6, name: 'Éowyn' },
+        { id: 5, name: 'Arwen' },
+      ],
+    },
+    {
+      name: 'Katie',
+    },
+  ];
+
+  test.deepEqual(actual, expected, 'LARP created');
+  test.end();
+});
+
+tape('Create a Character', function(test) {
+  const store = createStore(reducer, initialState);
+
+  store.dispatch({
+    type: 'CREATE_CHARACTER',
+    payload: { name: 'Katie', larp: 3 },
+  });
+
+  const actual = store.getState().larps[1];
+
+  const expected = {
+    id: 3,
+    name: "Elves 'n' Things",
+    ownerId: 1,
+    characters: [
+      { id: 4, name: 'Galadriel' },
+      { id: 5, name: 'Elrond' },
+      { id: 6, name: 'Éowyn' },
+      { id: 5, name: 'Arwen' },
+      { name: 'Katie' },
+    ],
+  };
+  test.deepEqual(actual, expected, 'Character created');
+  test.end();
+});
