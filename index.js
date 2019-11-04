d083eb90802db9994951d66f6c900da6fb21ffc8
Katie Wright <katie.wrght@gmail.com>
2019-11-04 12:22:34 +0100

    write reducer functions for new character actions

--- index.js
+++ index.js
@@ -39,6 +39,18 @@ const reducer = produce((state = initialState, action) => {
     case 'CREATE_LARP':
       state.larps.push(action.payload);
       break;
+    case 'CREATE_CHARACTER':
+      const id = action.payload.larp;
+      const larpIdx = state.larps.findIndex(l => l.id === id);
+      state.larps[larpIdx].characters.push({
+        name: action.payload.name,
+      });
+      break;
+    case 'DELETE_CHARACTER':
+      state.larps.map(
+        l => (l.characters = l.characters.filter(c => c.id !== action.payload))
+      );
+      break;
     default:
       return state;
   }
