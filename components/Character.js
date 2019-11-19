50e0b6006951483aa5b79328656a967280941be2
Katie Wright <katie.wrght@gmail.com>
2019-11-19 13:25:34 +0100

    rename character component

--- components/Character.js
+++ components/Character.js
@@ -3,20 +3,20 @@ import PropTypes from 'prop-types';
 import { CharacterSection, CharacterArticle } from './styles/Characters';
 import DeleteCharacter from './DeleteCharacter';
 
-const CharacterList = props => {
+const Character = props => {
   const { character } = props;
   return (
     <CharacterSection>
       <CharacterArticle>
         <h3>{character.name}</h3>
-        <DeleteCharacter character={character.id} />
+        <DeleteCharacter character={character.id} key={character.id} />
       </CharacterArticle>
     </CharacterSection>
   );
 };
 
-CharacterList.propTypes = {
+Character.propTypes = {
   character: PropTypes.object,
 };
 
-export default CharacterList;
+export default Character;
