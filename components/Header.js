b7014c9e9c085b0364bc614a344a1342009a98ba
Katie Wright <katie.wrght@gmail.com>
2019-11-21 12:20:34 +0100

    update header with route links

--- components/Header.js
+++ components/Header.js
@@ -1,31 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
 import NavBar from './styles/NavBar';
 
-const Header = props => {
-  const { user } = props;
-  return (
-    <NavBar>
-      <ul>
-        <li>
-          <Link to="/">
-            <strong>Larping 2019</strong>
-          </Link>
-        </li>
-        <li>
-          <Link to="/larps">Larps</Link>
-        </li>
-        <li>
-          <p>You are logged in as {user.name}</p>
-        </li>
-      </ul>
-    </NavBar>
-  );
-};
-
-Header.propTypes = {
-  user: PropTypes.object,
-};
+const Header = () => (
+  <NavBar>
+    <ul>
+      <li>
+        <Link to="/">
+          <strong>Larping 2019</strong>
+        </Link>
+      </li>
+      <li>
+        <Link to="/larps">Larps</Link>
+      </li>
+    </ul>
+  </NavBar>
+);
 
 export default Header;
