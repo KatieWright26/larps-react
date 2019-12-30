40dce9bcf603bb35e511ebbcc2767b9bbd54b680
Katie Wright <katie.wrght@gmail.com>
2019-12-30 13:55:28 +0100

    update create larp to functional component and make us of new useForm hook

--- components/CreateLarp.js
+++ components/CreateLarp.js
@@ -1,72 +1,53 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Form from './styles/Form';
+import { useForm } from './hooks/useForm';
 
-class CreateLarp extends Component {
-  constructor(props) {
-    super(props);
+function CreateLarp({ createLarp }) {
+  const [values, handleChange] = useForm({
+    name: '',
+    description: '',
+    location: '',
+    startDate: '',
+  });
 
-    this.state = {
-      name: '',
-      description: '',
-      location: '',
-      startDate: '',
-    };
-
-    this.saveToState = e => {
-      this.setState({ [e.target.name]: e.target.value });
-    };
-  }
-
-  render() {
-    const { createLarp } = this.props;
-    const { name, description, location, startDate } = this.state;
-
-    return (
-      <Form
-        onSubmit={e => {
-          e.preventDefault();
-          createLarp(this.state);
-          this.setState({
-            name: '',
-            description: '',
-            startDate: '',
-            location: '',
-          });
-        }}
-      >
-        <input
-          type="text"
-          name="name"
-          placeholder="Enter LARP name"
-          onChange={this.saveToState}
-          value={name}
-        />
-        <input
-          type="text"
-          name="location"
-          placeholder="Enter a location"
-          onChange={this.saveToState}
-          value={location}
-        />
-        <input
-          type="date"
-          name="startDate"
-          onChange={this.saveToState}
-          value={startDate}
-        />
-        <textarea
-          rows="7"
-          name="description"
-          placeholder="Enter description for LARP"
-          onChange={this.saveToState}
-          value={description}
-        />
-
-        <input type="submit" />
-      </Form>
-    );
-  }
+  return (
+    <Form
+      onSubmit={e => {
+        e.preventDefault();
+        createLarp(values);
+      }}
+    >
+      <input
+        type="text"
+        name="name"
+        placeholder="Enter LARP name"
+        onChange={handleChange}
+        value={values.name}
+      />
+      <input
+        type="text"
+        name="location"
+        placeholder="Enter a location"
+        onChange={handleChange}
+        value={values.location}
+      />
+      <input
+        type="date"
+        name="startDate"
+        onChange={handleChange}
+        value={values.startDate}
+      />
+      <textarea
+        rows="7"
+        name="description"
+        placeholder="Enter description for LARP"
+        onChange={handleChange}
+        value={values.description}
+      />
+      <input type="submit" />
+    </Form>
+  );
 }
 
 CreateLarp.propTypes = {
